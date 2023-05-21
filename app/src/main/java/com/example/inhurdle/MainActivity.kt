fd3343b961b0653b70cb51e0673674771ac69a75
eujin Lee <eireh001@naver.com>
2023-05-21 22:21:26 +0900

    Initial commit

---
+++ app/src/main/java/com/example/inhurdle/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.inhurdle
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+
+    lateinit var camera_btn : ImageButton
+    lateinit var use_btn : ImageButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+        camera_btn = findViewById(R.id.Camera_btn)
+
+    }
+
+
+}
